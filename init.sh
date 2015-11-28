f99ea255e6997d22e42cfbcf825d2779358f5b58
wanqian <wanqian@conew.com>
2015-11-29 01:08:29 +0800

    box initializer

---
+++ init.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+root=$(pwd)
+
+cat bashrc > ${HOME}/.bashrc
+
+cat gitconfig > ${HOME}/.gitconfig
+
+# emacs
+wget 'http://ftp.twaren.net/Unix/GNU/gnu/emacs/emacs-24.5.tar.gz'
+tar xf emacs-24.5.tar.gz
+sudo yum install ncurses-devel -y
+cd $root/emacs-24.5/
+./configure
+sudo make install
+rm -rf ${HOME}/.emacs*
+cd ${HOME}
+git clone https://github.com/qianwan/emacs.d.git
+
+cd $root
